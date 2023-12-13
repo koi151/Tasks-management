c9593a056ac4d037dca97a8f7c1cb894e98a5215
Khôi Nguyễn <originalkhoi151@gmail.com>
2023-12-13 12:20:07 +0700

    add pagination and sorting api

--- api/v1/controllers/tasks.controller.js
+++ api/v1/controllers/tasks.controller.js
@@ -1,19 +1,40 @@
 const Task = require('../model/tasks.model');
 
+const paginationHelper = require('../../../helpers/pagination');
+
 // [GET] /api/v1/tasks/
 module.exports.index = async (req, res) => {
   try {
     const criterias = {
       deleted: false
     }
-  
+
+    // SORT
+    const sort = {}
+
     if (req.query.status) {
       criterias.status = req.query.status
     }
-  
-    const tasks = await Task.find(criterias) 
-  
+
+    if (req.query.sortKey && req.query.sortValue) {
+      sort[req.query.sortKey] = req.query.sortValue;
+    }
+    // PAGINATION
+    let initPagination = {
+      currentPage: 1,
+      limitItems: 2
+    }
+
+    const tasksCount = await Task.countDocuments(criterias);
+    const paginationObj = paginationHelper(initPagination, req.query, tasksCount);
+
+    const tasks = await Task.find(criterias)
+      .sort(sort)
+      .limit(paginationObj.limitItems)
+      .skip(paginationObj.skip);
+
     res.json(tasks)
+
   } catch (error) {
     console.log('Error occured:', error);
     res.json('Not found');
@@ -27,7 +48,7 @@ module.exports.detail = async (req, res) => {
       _id: req.params.id,
       deleted: false
     })
-  
+
     res.json(task);
   } catch (error) {
     console.log('Error occured:', error);
